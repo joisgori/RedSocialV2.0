479baaaba60acc44c8d6f5168c7568aa0222112f
luist23 <00127717@uca.edu.sv>
2018-11-26 15:08:55 -0800

    modelos de past commentarios y su controlador( todavia falta obv.. :V)

---
+++ controller/principalControler.js
@@ -0,0 +1,189 @@
+var mongoose = require('mongoose');
+var postingModel = require('../models/postings');
+var commitModel = require('../models/commits');
+
+
+let controller = {};
+
+// Obtener todos los usuarios
+controller.getAll = function (req, res) {
+    postingModel.find({}, function (err, posts) {
+        if (err) {
+            res.status(500);
+            res.json({
+                ok: false,
+                err
+            });
+        } else {            
+            res.json({
+                ok: true,
+                posts,
+                getcommit
+            });
+        }
+    });
+};
+
+controller.getOne = function (req, res) {
+    postingModel.findOne({
+        _id: req.params.id
+    }, function (err, post) {
+        if (err) {
+            res.status(500);
+            res.json({
+                ok: false,
+                err
+            });
+        } else {
+            res.json({
+                ok: true,
+                post,
+                getcommit
+            });
+        }
+    });
+};
+
+controller.update = function (req, res) {
+    // Falta validar si exiten los atributos
+    let update = {
+        post: req.body.post
+    };
+    postingModel.findByIdAndUpdate(req.params.id, update, function (err, old) {
+        if (err) {
+            res.status(500);
+            res.json({
+                ok: false,
+                err
+            })
+        } else {
+            res.json({
+                ok: true,
+                old,
+                update
+            });
+        }
+    });
+};
+
+controller.insert = function(req,res){
+    let userNew = new postingModel({
+        post: req.body.post,
+        username: req.body.username
+    });
+
+    userNew.save(function(err,insertado){
+        if(err){
+            res.status(500);
+            res.json({
+                ok: false,
+                err
+            });
+        } else {
+            res.json({
+                ok: true,
+                insertado
+            });
+        }
+    });
+};
+
+controller.delete =  function(req,res){
+    postingModel.findByIdAndRemove(req.params.id, function(err, eliminado){
+        if(err){
+            res.status(500);
+            res.json({
+                ok: false,
+                err
+            });
+        } else {
+            res.json({
+                ok: true,
+                eliminado
+            });
+        }
+    });
+}
+
+controller.insertCommit = function(req,res){
+    let commitNew = new commitModel({
+        commit: req.body.commit,
+        username: req.body.username
+    });
+
+    commitNew.save(function(err,insertado){
+        if(err){
+            res.status(500);
+            res.json({
+                ok: false,
+                err
+            });
+        } else {
+            res.json({
+                ok: true,
+                insertado
+            });
+        }
+    });
+};
+
+controller.deleteCommit =  function(req,res){
+    commitModel.findByIdAndRemove(req.params.id, function(err, eliminado){
+        if(err){
+            res.status(500);
+            res.json({
+                ok: false,
+                err
+            });
+        } else {
+            res.json({
+                ok: true,
+                eliminado
+            });
+        }
+    });
+}
+
+
+
+
+module.exports = controller;
+
+getcommit = function(req){
+    postingModel.findById(req, function (err, commits) {
+        if (err) {
+            return err;
+        } else {            
+            return commits;
+        }
+    });
+}
+
+
+
+
+
+
+/**imagenes
+ * 
+var upload = multer({dest: "./uploads"});
+var mongo = require('mongodb');
+var Grid = require("gridfs-stream");
+Grid.mongo = mongo;
+
+router.post('/:id', upload.array('photos', 200), function(req, res, next){
+gfs = Grid(db);
+var ss = req.files;
+   for(var j=0; j<ss.length; j++){
+     var originalName = ss[j].originalname;
+     var filename = ss[j].filename;
+     var writestream = gfs.createWriteStream({
+         filename: originalName
+     });
+    fs.createReadStream("./uploads/" + filename).pipe(writestream);
+   }
+});
+
+<form action="/" method="post" enctype="multipart/form-data">
+<input type="file" name="photos">
+ */
--- routes/principal.js
+++ routes/principal.js
@@ -7,6 +7,20 @@ router.get('/', function(req, res, next) {
   res.render('principal');
 });
 
+router.get('/', UserController.getAll);
+router.get('/:id', UserController.getOne);
+// Create
+router.post('/', UserController.insert);
+
+// UPDATE
+router.put('/:id', UserController.update);
+
+// Delete
+router.delete('/:id',UserController.delete);
+
+module.exports = router;
+
+
 /*
 //Patrón para crear las rutas
 router.get('/signup', function(req, res, next) { //Cambiaré la ruta de signup
