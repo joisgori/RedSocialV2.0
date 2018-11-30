f9be3fb161f964277efb12085f7a55bfb0721ef2
Javier Martinez <00034315@uca.edu.sv>
2018-11-30 16:19:16 -0600

    Config

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports ={
-    'url': "mongodb://localhost/BFriendsLogin" //nombre de la bases de datos 
+    'url': "mongodb://Luis:aBcd1234@ds239940.mlab.com:39940/bfriends" //nombre de la bases de datos 
 
 };
